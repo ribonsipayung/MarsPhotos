9e93e41c77d05292a570577f7b9ad3b00c9674fa
ribonsipayung <ribonsipayung412@gmail.com>
2023-12-06 00:59:26 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.ribonsipayung.marsphotos"
+    namespace = "com.ribonsipayung.weatherinformation"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.ribonsipayung.marsphotos"
+        applicationId = "com.ribonsipayung.weatherinformation"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -70,6 +70,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MarsPhotos"
+rootProject.name = "WeatherInformation"
 include(":app")
  
